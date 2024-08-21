7e5d215725038f150191001212611d4165591e09
CharlesKibet <charlesbiegon973@gmail.com>
2024-08-21 10:27:56 +0300

    my data updated

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'I am a full-stack developer who is passionate about building cool things on the web.I do major mostly on  HTML, CSS(Tailwind) , JavaScript(React.js) for frontend application and Python(FastApi,Flask, Django) ,DBMS (PostgreSql,Sqlite) for the backend application.For mobile application I work with React-Native and Figma for UI design',
+	},
+	{
+		id: 2,
+		bio: 'With a certificate from Moringa School and some years of self taught experience, I have built valuable experience in software development. I am also a curious guy who is really ready to learn.So are you looking for a developer for your next big project? Hit me up.',
+	},
+];
---
+++ src/data/clientsData.js
@@ -0,0 +1,54 @@
+// Import images
+import HtmlImage from '../images/brands/html.png';
+import CssImage from '../images/brands/css.png';
+import JavaScriptImage from '../images/brands/javascript.png';
+import ReactImage from '../images/brands/react.png';
+import Tailwindmage from '../images/brands/tailwind.png';
+import Python from '../images/brands/pthon.webp';
+import PostgreSQL from '../images/brands/ps.webp';
+import NextImage from '../images/brands/nextjs.png';
+
+export const clientsHeading = 'Technologies';
+
+export const clientsData = [
+	{
+		id: 1,
+		title: 'Amazon',
+		img: HtmlImage,
+	},
+	{
+		id: 2,
+		title: 'Sony',
+		img: CssImage,
+	},
+	{
+		id: 3,
+		title: 'Adidas',
+		img: JavaScriptImage,
+	},
+	{
+		id: 4,
+		title: 'FILA',
+		img: ReactImage,
+	},
+	{
+		id: 5,
+		title: 'NB',
+		img: PostgreSQL,
+	},
+	{
+		id: 6,
+		title: 'SAMSUNG',
+		img: Tailwindmage,
+	},
+	{
+		id: 7,
+		title: 'CANON',
+		img: Python,
+	},
+	{
+		id: 8,
+		title: 'PUMA',
+		img: NextImage,
+	},
+];

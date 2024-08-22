e3cdabb59f94ef5235cfce8d24cd47c63b651188
CharlesKibet <charlesbiegon973@gmail.com>
2024-08-22 11:04:39 +0300

    Resume shifted to the public now

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -49,9 +49,9 @@ const AppBanner = () => {
 					}}
 					className="flex justify-center sm:block"
 				>
-					<a
-						
-						href="/src/assets/files/Resume.pdf" download={'Resume.pdf'} 
+						<a
+						download="Resume.pdf"
+						href="/files/Resume.pdf"
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 						aria-label="Download Resume"
 					>
